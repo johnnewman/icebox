1f48c24fc284c620292f603c43449a060d4f2168
John Newman <newman719@gmail.com>
2020-08-01 14:04:49 -0400

    Updating version number.

--- __main__.py
+++ __main__.py
@@ -19,7 +19,7 @@ import time
 __author__ = "John Newman"
 __copyright__ = "Copyright 2020, John Newman"
 __license__ = "MIT"
-__version__ = "1.1.0"
+__version__ = "1.0.0"
 __maintainer__ = "John Newman"
 __status__ = "Production"
 
